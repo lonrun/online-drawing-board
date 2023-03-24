6e15c75f67b4eeb304c4152431199d74120c0ee9
lonrun <lonrun@126.com>
2023-03-24 21:39:03 +0800

    Add MongoDB support for storing drawings

--- server.js
+++ server.js
@@ -1,30 +1,59 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname));
+const mongoUrl = 'mongodb://localhost:27017';
+const dbName = 'drawingBoard';
+const collectionName = 'drawings';
 
-io.on('connection', (socket) => {
+async function init() {
+  const client = new MongoClient(mongoUrl, { useUnifiedTopology: true });
+  await client.connect();
+
+  const db = client.db(dbName);
+  const drawingsCollection = db.collection(collectionName);
+
+  app.use(express.static(__dirname));
+
+  io.on('connection', (socket) => {
     console.log('user connected:', socket.id);
 
-    socket.on('drawing-data', (data) => {
-        socket.broadcast.emit('drawing-data', data);
+    socket.on('drawing-data', async (data) => {
+      await saveDrawing(socket.id, data);
+      socket.broadcast.emit('drawing-data', data);
     });
 
     socket.on('start-drawing', (data) => {
-        socket.broadcast.emit('start-drawing', data);
+      socket.broadcast.emit('start-drawing', data);
     });
 
     socket.on('disconnect', () => {
-        console.log('user disconnected:', socket.id);
+      console.log('user disconnected:', socket.id);
     });
-});
+  });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
+  async function saveDrawing(userId, drawingData) {
+    const result = await drawingsCollection.insertOne({ userId, drawingData });
+    console.log("Drawing saved:", result.insertedId);
+  }
+
+  app.get('/drawings/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    const drawings = await drawingsCollection.find({ userId }).toArray();
+    res.json(drawings);
+  });
+
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
+  });
+}
+
+init().catch((error) => {
+  console.error("Unable to connect to MongoDB.", error);
 });
